bf58f6c46974496daf2825cee587aeb1e4e88320
natsu <nyny.sugar@gmail.com>
2022-09-02 22:35:00 +0900

    css:.top__image作成

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 "use strict";
+
+// .top__image slick
+$(".top__list").slick({
+  autoplay: true,
+  autoplaySpeed: 3000,
+  speed: 1000,
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: true,
+  prevArrow: '<div class="top__button-prev"></div>',
+  nextArrow: '<div class="top__button-next"></div>',
+  dots: true,
+  pauseOnFocus: false,
+  pauseOnHover: false,
+  pauseOnDotsHover: false,
+  centerMode: true,
+  centerPadding: "10%",
+});
+
+//スマホ用：スライダーをタッチしても止めずにスライドをさせたい場合
+$(".top__list").on(
+  "touchmove",
+  function (event, slick, currentSlide, nextSlide) {
+    $(".top__list").slick("slickPlay");
+  }
+);
